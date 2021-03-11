06bdd3d553d85a42551ce1bca28496d9d2228c97
burakaltunoren <77547087+BurakAlt@users.noreply.github.com>
2021-03-11 21:47:20 +0300

    Customer Manager operasyonları yazıldı

--- Business/Concrete/CustomerManager.cs
+++ Business/Concrete/CustomerManager.cs
@@ -1,4 +1,8 @@
 ﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +11,47 @@ namespace Business.Concrete
 {
     public class CustomerManager : ICustomerService
     {
+        ICustomerDal _customerDal;
+
+        public CustomerManager(ICustomerDal customerDal)
+        {
+            _customerDal = customerDal;
+        }
+
+        public IResult Add(Customer customer)
+        {
+            if (customer.City.Length < 2)
+            {
+                return new ErrorResult(Messages.InvalidCityName);
+            }
+            _customerDal.Add(customer);
+            return new SuccessResult(Messages.CustomerAdded);
+        }
+
+        public IResult Delete(Customer customer)
+        {
+            _customerDal.Delete(customer);
+            return new SuccessResult(Messages.CustomerDeleted);
+        }
+
+        public IDataResult<List<Customer>> GetAll()
+        {
+            if (DateTime.Now.Hour == 1)
+            {
+                return new ErrorDataResult<List<Customer>>(Messages.MaintenanceTime);
+            }
+            _customerDal.GetAll();
+            return new SuccessDataResult<List<Customer>>(_customerDal.GetAll(), Messages.CustomerListed);
+        }
+
+        public IResult Update(Customer customer)
+        {
+            if (DateTime.Now.Hour == 1)
+            {
+                return new ErrorResult(Messages.MaintenanceTime);
+            }
+            _customerDal.Update(customer);
+            return new SuccessResult(Messages.CustomerUpdated);
+        }
     }
 }
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,10 @@ namespace Business.Constants
         public static string InvalidProductName = "Ürün adı geçersiz";
         public static string MaintenanceTime = "Sistem bakımda.";
         public static string ProductsListed = "ürünler listelendi";
+        public static string CustomerAdded = "Müşteri Eklendi";
+        public static string InvalidCityName = "Geçersiz şehir adı";
+        public static string CustomerDeleted = "Müşteri silindi";
+        public static string CustomerListed = "Müşteriler Listelendi";
+        public static string CustomerUpdated = "Müşteri Bilgileri Güncellendi";
     }
 }
