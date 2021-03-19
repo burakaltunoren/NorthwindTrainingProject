d7732a91e60b2907a6906645a38f055cb2e60965
burakaltunoren <77547087+BurakAlt@users.noreply.github.com>
2021-03-19 18:03:38 +0300

    Validation için Refactoring_OK

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -18,4 +18,3 @@ namespace Business.DependencyResolvers.Autofac
         }
     }
 }
- 
---
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty();
+            RuleFor(p => p.ProductName).MinimumLength(2);
+            RuleFor(p => p.UnitPrice).NotEmpty();
+            RuleFor(p => p.UnitPrice).GreaterThan(0);
+            RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1);
+            RuleFor(p => p.ProductName).Must(StartWithA).WithMessage("Ürün adı 'A' ile başlamalıdır...!");
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");     // burası ya true ya da false döner. Dönen değere göre de kural ya çalışır ya da hata verir.
+        }
+    }
+}
